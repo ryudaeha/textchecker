70aff71919c99151aceb51f4ec8a6ef24d0749d1
daeha.ryu <daeha.ryu@samsungds.net>
2024-06-13 15:53:42 +0900

    [feature] add compare str length chk

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 class check:
-    def check_len(self):
-        return 1
+    def check_len(self, str_1, str_2):
+        return 60
     def check_alpha(self):
         return 1
 
--- test_checker.py
+++ test_checker.py
@@ -10,7 +10,7 @@ class Chk(TestCase):
         super().setUp()
 
     def test_length(self):
-        self.assertEqual(1,self.chk.check_len())
+        self.assertEqual(1,self.chk.check_len('ASD', 'DSA'))
 
     def test_alpha(self):
         self.assertEqual(1,self.chk.check_alpha())
@@ -19,4 +19,4 @@ class Chk(TestCase):
         self.assertEqual(1, self.chk.score('123','123'))
 
     def test_length_compare_1(self):
-        self.assertEqual(1,self.chk.check_len())
+        self.assertEqual(60,self.chk.check_len('ASD', 'DSA'))
